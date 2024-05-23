07d12ceda354490354e940e27c67badbf699edc6
hong-tm <asuszen48319@gmail.com>
2024-05-24 04:42:44 +0800

    🪛Update font URL in MaterialDesign.scss

--- src/ts/createMD/CreateMD.ts
+++ src/ts/createMD/CreateMD.ts
@@ -1,9 +1,11 @@
 export class CreateMD {
     public getmd(code: string, className: string) {
         const md = document.createElement("div")
+
         md.classList.add("material-symbols-rounded")
         md.classList.add(className)
         md.innerHTML = `&#x${code};`
+
         return md
     }
 }
